eb0a1c31e570c45336a4ec4b3f94dbdef6d02482
MrMeepson <43138837+MrMeepson@users.noreply.github.com>
2019-07-30 00:43:45 -0400

    Add files via upload

--- TeamInfoScrollStyle.js
+++ TeamInfoScrollStyle.js
@@ -4,7 +4,6 @@ var infoBoxes = document.getElementsByClassName('TeamMemberInfo');
 
 for (var i = 0; i < infoBoxes.length; i++) {
     infoBoxes[i].onscroll = function() { ApplyStyles() };
-    console.log(infoBoxes.length + " // " + i + " // " + infoBoxes[i].innerHTML);
 }
 
 function ApplyStyles () {
@@ -14,12 +13,15 @@ function ApplyStyles () {
     
     var infoBoxes = document.getElementsByClassName('TeamMemberInfo');
     
+    var emSize = parseFloat(getComputedStyle(infoBoxes[0]).fontSize);
+    var paddingSize = emSize * 5.5;
+    
     for (var j = 0; j < infoBoxes.length; j++) {
         if (infoBoxes[j].scrollHeight != infoBoxes[j].clientHeight) {
-            if (infoBoxes[j].scrollTop + 80 >= infoBoxes[j].scrollHeight) {
+            if (infoBoxes[j].scrollTop + paddingSize >= infoBoxes[j].scrollHeight) {
                 infoBoxes[j].style.boxShadow = topBorder;
             }
-            else if (infoBoxes[j].scrollTop + 80 < infoBoxes[j].scrollHeight) {
+            else if (infoBoxes[j].scrollTop + paddingSize < infoBoxes[j].scrollHeight) {
                 infoBoxes[j].style.boxShadow = bottomBorder;
             }
             else {
@@ -27,24 +29,4 @@ function ApplyStyles () {
             }
         }
     }
-}
-
-function UpdateStyles (object) {
-    
-    console.log(object);
-    
-    var topBorder = "0 7px 9px -7px whitesmoke inset";
-    var bottomBorder = "0 -7px 9px -7px whitesmoke inset";
-        
-    if (object.scrollHeight > object.clientHeight) {
-        if (object.scrollHeight === object.scrollTop) {
-            object.style.boxShadow = topBorder;
-        }
-        else if (0 === object.scrollTop) {
-            object.style.boxShadow = bottomBorder;
-        }
-        else {
-            object.style.boxShadow = "none";
-        }
-    } 
 }

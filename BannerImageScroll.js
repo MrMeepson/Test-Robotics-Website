e1a2e7dd516e8680e6ba6fc55086513360cac179
MrMeepson <43138837+MrMeepson@users.noreply.github.com>
2019-08-04 00:47:31 -0400

    Add files via upload

--- BannerImageScroll.js
+++ BannerImageScroll.js
@@ -5,15 +5,51 @@ var bannerContainer = document.getElementsByClassName('image-banner-images')[0];
 
 var images = document.getElementsByClassName('image-banner-image-wrapper');
 
+var pxSizes = [750, 1000, 1250, 1500];
+
 backButton.onclick = function() {scrollBackward()};
 forwardButton.onclick = function() {scrollForward()};
+window.onresize = function() {calibrateDisplay()};
+window.onload = function() {calibrateDisplay()};
 
 function scrollBackward () {
     bannerContainer.prepend(images[images.length - 1]);
     images = document.getElementsByClassName('image-banner-image-wrapper');
+    calibrateDisplay();
 }
 
 function scrollForward () {
     bannerContainer.appendChild(images[0]);
     images = document.getElementsByClassName('image-banner-image-wrapper');
+    calibrateDisplay();
+}
+
+function calibrateDisplay () {
+    //Imitialize variables
+    var pageWidth = window.innerWidth;
+    var numberOfImagesToDisplay = 3;
+    
+    for (var i = 0; i < pxSizes.length; i++) {
+        if (pageWidth <+ pxSizes[i]) {
+            numberOfImagesToDisplay = i + 1;
+            break;
+        }
+        else {
+            numberOfImagesToDisplay = pxSizes.length;
+        }
+    }
+    
+    if (numberOfImagesToDisplay > images.length) {
+        numberOfImagesToDisplay = images.length;
+    }
+    
+    for (var j = 0; j < images.length; j++) {
+        if (j < numberOfImagesToDisplay) {
+            images[j].style.display = "flex";
+            images[j].style.width = (100 / numberOfImagesToDisplay) + "%";
+        }
+        else {
+            images[j].style.display = "none";
+        }
+    }
 }
